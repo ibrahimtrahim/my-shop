e19489dc76779078859d0446d111ba324d2415e7
ibrahimtrahim <trahimbrahim@gmail.com>
2024-03-30 11:19:29 +0000

    latest products

---
+++ frontend/src/products.js
@@ -0,0 +1,76 @@
+const products = [
+    {
+      _id: '1',
+      name: "Wireless Bluetooth Earbuds",
+      image: "/images/1001.jpg",
+      description: "True wireless earbuds with Bluetooth connectivity.",
+      brand: "TechSounds",
+      category: "Electronics",
+      price: 49.99,
+      countInStock: 100,
+      rating: 4.5,
+      numReviews: 120
+    },
+    {
+      _id: '2',
+      name: "Smartwatch Series 5",
+      image: "/images/1002.jpg",
+      description: "Stay connected with this stylish smartwatch.",
+      brand: "SmartWear",
+      category: "Electronics",
+      price: 149.99,
+      countInStock: 50,
+      rating: 4.8,
+      numReviews: 80
+    },
+    {
+      _id: '3',
+      name: "Portable Bluetooth Speaker",
+      image: "/images/1003.jpg",
+      description: "Enjoy high-quality sound on the go.",
+      brand: "SoundWave",
+      category: "Electronics",
+      price: 79.99,
+      countInStock: 80,
+      rating: 4.3,
+      numReviews: 95
+    },
+    {
+      _id: '4',
+      name: "Wireless Gaming Mouse",
+      image: "/images/1004.jpg",
+      description: "Ergonomic mouse for professional gamers.",
+      brand: "GameMaster",
+      category: "Electronics",
+      price: 69.99,
+      countInStock: 60,
+      rating: 4.6,
+      numReviews: 110
+    },
+    {
+      _id: '5',
+      name: "Stainless Steel Water Bottle",
+      image: "/images/1005.jpg",
+      description: "Keeps your drinks hot or cold for hours.",
+      brand: "HydroFlask",
+      category: "Kitchen",
+      price: 29.99,
+      countInStock: 120,
+      rating: 4.9,
+      numReviews: 150
+    },
+    {
+        _id: '6',
+        name: "Water Bottle",
+        image: "/images/1006.jpg",
+        description: "Keeps your drinks hot or cold for hours.",
+        brand: "HydroFlask",
+        category: "Kitchen",
+        price: 39.99,
+        countInStock: 70,
+        rating: 9.9,
+        numReviews: 130
+      }
+  ];
+
+  export default products;
