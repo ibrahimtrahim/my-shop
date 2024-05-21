7b1614bf33eab4c47d1f07fcaf6cb90a618ed63a
ibrahimtrahim <trahimbrahim@gmail.com>
2024-05-21 22:29:52 +0000

    shopping cart

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,10 @@
-import { Navbar, Nav, Container, NavbarToggle } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container, NavbarToggle } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import {LinkContainer} from 'react-router-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 
 function Header() {
+    const { cartItems } = useSelector((state) => state.cart );
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -14,7 +16,15 @@ function Header() {
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
                         <LinkContainer to='/cart'>
-                        <Nav.Link><FaShoppingCart />Cart</Nav.Link>
+                        <Nav.Link><FaShoppingCart />Cart
+                            {
+                                cartItems.length > 0 && (
+                                    <Badge pill bg='success' style={{marginLeft: '5px'}}>
+                                        { cartItems.reduce((a, c) => a + c.qty, 0 )}
+                                    </Badge>
+                                )
+                            }
+                        </Nav.Link>
                         </LinkContainer>
                         <LinkContainer to='/login'>
                             <Nav.Link><FaUser />Sign In</Nav.Link>
