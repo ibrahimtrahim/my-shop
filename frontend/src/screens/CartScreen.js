6e5a52fffbc7657c717b48afc32b9278fb7cc846
ibrahimtrahim <trahimbrahim@gmail.com>
2024-05-23 22:22:54 +0000

    remove button

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, ListGroup, Image, Form, Card, Button} from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 import Message from '../components/Message';
-import { addToCart } from '../slices/cartSlice';
+import { addToCart, removeFromCart } from '../slices/cartSlice';
 
 
 function CartScreen() {
@@ -15,7 +15,16 @@ function CartScreen() {
 
     const addToCartHandler = async (product, qty) => {
         dispatch(addToCart({...product, qty}))
+    };
+
+    const removeFromCartHandler = async (id) => {
+        dispatch(removeFromCart(id))
+    };
+
+    const checkoutHandler = () => {
+        navigate('/login?redirect=/shipping')
     }
+
   return (
     <Row>
         <Col md={8}>
@@ -52,7 +61,7 @@ function CartScreen() {
                                     </Form.Control>
                                 </Col>
                                 <Col md={2}>
-                                    <Button type='button' variant='light'>
+                                    <Button type='button' variant='light' onClick={() => removeFromCartHandler(item._id)}>
                                         <FaTrash/>
                                     </Button>
                                 </Col>
@@ -72,7 +81,12 @@ function CartScreen() {
                         ${ cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2) }
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        <Button type='button' className='btn-block' disabled={cartItems.length === 0}>
+                        <Button 
+                            type='button' 
+                            className='btn-block' 
+                            disabled={cartItems.length === 0}
+                            onClick={checkoutHandler}
+                            >
                             Proceed To Checkout
                         </Button>
                     </ListGroup.Item>
